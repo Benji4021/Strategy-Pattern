69af591467af93053e33e1004bc00c8cf14c6329
Benji4021 <114071368+Benji4021@users.noreply.github.com>
2023-04-22 15:16:31 +0200

    Bubblesort geht nicht

---
+++ Numbers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Strategy_Pattern;
+
+namespace Strategy_Pattern
+{
+    public class Numbers
+    {
+        public static void Fill(int[] arr)
+        {
+            Random rnd = new Random();
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(0, 100);
+            }
+        }
+
+        public static void PrintArr(int[]arr)
+        {
+            foreach(int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void Algorithm(int[] arr)
+        {
+            string path = Console.ReadLine();
+            bool A = true;
+
+            while (A)
+                switch (path)
+                {
+                    case "A":
+                        
+                        A = false;
+
+                        break;
+
+                    case "B":
+                        A = false;
+                        break;
+
+                    case "C":
+                        A = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("That is not an Option.");
+                        break;
+                }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using Strategy_Pattern;
+
+//while (true) { }
+
+Console.WriteLine("How big should the array be?");
+
+int size;
+
+while (!int.TryParse(Console.ReadLine(), out size))
+{
+    Console.WriteLine("Please enter a valid integer value.");
+}
+
+int[] arr = new int[size];
+
+Numbers.Fill(arr);
+Numbers.PrintArr(arr);
+
+Console.WriteLine("A: Bubblesort");
+Console.WriteLine("B: Selectionsort");
+Console.WriteLine("C: Insertionsort");
+
+Numbers.Algorithm(arr);
+Numbers.PrintArr(arr);
+
